52a7b42b969632125246bdf6eb746fd5602b44c3
Aamir Abro <aamir@venturedive.com>
2016-04-14 02:21:54 +0500

    converted app to express js

--- app.js
+++ app.js
@@ -3,52 +3,73 @@ const http         = require('http'),
       path         = require('path'),
       contentTypes = require('./utils/content-types'),
       sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+      env          = process.env,
+      express      = require('express');
 
-let fbAppToken = process.env.FB_APP_TOKEN;
+let fbAppToken = env.FB_APP_TOKEN;
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
+var app = express();
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+app.use('/static', express.static('static'));
 
-  if (url == '/health') {
-    res.writeHead(200);
+app.get('/health', function(req, res){
+  res.writeHead(200);
     res.end();
-  } else if (url.indexOf('/info/') == 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else if (url.indexOf('helloworld') >= 0) {
-    res.end("helloworld");
-  }else if (url.indexOf('/facebookmessenger') == 0) {
-    if (req.query['hub.verify_token'] === fbAppToken) {
-      res.send(req.query['hub.challenge']);
-    } else {
-      res.send('Error, wrong validation token');
-    }
-  }
-  else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end();
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
-      }
-    });
-  }
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+app.get('/info', function(req, res){
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
+  res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+});
+
+app.get('/helloworld', function(req, res){
+  res.end("helloworld");
+});
+
+
+
+// let server = http.createServer(function (req, res) {
+//   let url = req.url;
+//   if (url == '/') {
+//     url += 'index.html';
+//   }
+
+//   // IMPORTANT: Your application HAS to respond to GET /health with status 200
+//   //            for OpenShift health monitoring
+
+//   if (url == '/health') {
+//     res.writeHead(200);
+//     res.end();
+//   } else if (url.indexOf('/info/') == 0) {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.setHeader('Cache-Control', 'no-cache, no-store');
+//     res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+//   } else if (url.indexOf('helloworld') >= 0) {
+//     res.end("helloworld");
+//   }
+//   else {
+//     fs.readFile('./static' + url, function (err, data) {
+//       if (err) {
+//         res.writeHead(404);
+//         res.end();
+//       } else {
+//         let ext = path.extname(url).slice(1);
+//         res.setHeader('Content-Type', contentTypes[ext]);
+//         if (ext === 'html') {
+//           res.setHeader('Cache-Control', 'no-cache, no-store');
+//         }
+//         res.end(data);
+//       }
+//     });
+//   }
+// });
+
+// server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+//   console.log(`Application worker ${process.pid} started...`);
+// });
+
+
+http.createServer(app).listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
