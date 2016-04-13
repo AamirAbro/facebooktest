5730a9b6797d2149d793c7bb34564662e02d9622
Aamir Abro <aamir@venturedive.com>
2016-04-14 02:44:27 +0500

    added echo back messenger bot

--- app.js
+++ app.js
@@ -27,12 +27,26 @@ app.get('/helloworld', function(req, res){
   res.end("helloworld");
 });
 
-app.get('/facebookwebhook', function (req, res) {
-  // if (req.query['hub.verify_token'] === fbAppToken) {
-    res.send(req.query['hub.challenge']);
-  // }
-  // res.send('Error, wrong validation token');
-})
+// for Authorization
+// app.get('/facebookwebhook', function (req, res) {
+//   // if (req.query['hub.verify_token'] === fbAppToken) {
+//     res.send(req.query['hub.challenge']);
+//   // }
+//   // res.send('Error, wrong validation token');
+// })
+
+app.post('/facebookwebhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+    }
+  }
+  res.sendStatus(200);
+});
 
 
 
@@ -80,3 +94,27 @@ app.get('/facebookwebhook', function (req, res) {
 http.createServer(app).listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
+
+
+
+var token = env.FB_PAGE_TOKEN;
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
